d33210ed4cc74ba28d4ccdcdb59d4e37c14dbad8
Tiago Marchetti Lyra <tiago.mlyra@gmail.com>
2023-05-23 20:55:42 -0300

    Commit Exercícios 3 e 4
    Moedas coletadas e contadas.

--- ProgrammingCP03/Assets/Scripts/Player.cs
+++ ProgrammingCP03/Assets/Scripts/Player.cs
@@ -9,10 +9,13 @@ public class Player : MonoBehaviour
     [Header("Player Variables")]
     [SerializeField] private float velocity;
 
+    private int coinsCollected;
+
     // Start is called before the first frame update
     private void Awake()
     {
         playerTransform = GetComponent<Transform>();
+        coinsCollected = 0;
     }
 
     // Update is called once per frame
@@ -26,6 +29,15 @@ public class Player : MonoBehaviour
         float moveX = Input.GetAxisRaw("Horizontal") * velocity * Time.deltaTime;
         float moveY = Input.GetAxisRaw("Vertical") * velocity * Time.deltaTime;
         playerTransform.Translate(new Vector3(moveX, moveY));
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Coin")
+        {
+            coinsCollected++;
+            print("Foram coletadas " + coinsCollected + " moedas.");
+        }
     }
 
 }
